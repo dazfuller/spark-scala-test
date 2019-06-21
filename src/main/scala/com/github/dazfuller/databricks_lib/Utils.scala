0d0aa755e361dbecc525ded6cf1050b5f8ea47ee
Darren Fuller <darren@elastacloud.com>
2019-06-21 08:45:39 +0100

    add code comments

--- src/main/scala/com/github/dazfuller/databricks_lib/Utils.scala
+++ src/main/scala/com/github/dazfuller/databricks_lib/Utils.scala
@@ -4,10 +4,22 @@ import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions.{col, row_number}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
+/**
+  * Defines a set of utilities for working with [[org.apache.spark.sql.DataFrame]]
+  *
+  * @param spark The spark session to use, if not specified the current spark session is retrieved
+  */
 class Utils(spark: SparkSession = SparkSession.builder().getOrCreate()) {
 
   import spark.implicits._
 
+  /**
+    * Filters a data frame to return only the latest records
+    *
+    * @param df              The data frame to filter
+    * @param idColumn        the name of the column containing the primary identifier (key) in the data frame
+    * @param timestampColumn name of the column which identifies the date and time of the change
+    */
   def latestRecords(df: DataFrame, idColumn: String, timestampColumn: String): DataFrame = {
     val recordWindow = Window.partitionBy(col(idColumn)).orderBy(col(timestampColumn).desc)
 
